4554c74b885ab01f7919b2873cfaa316d3e61bb0
JasonNCF <u3568695@connect.hku.hk>
2023-05-12 17:40:10 +0800

    edited eval.py

--- eval.py
+++ eval.py
@@ -1,3 +1,21 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import os, json, cv2, random
+import matplotlib.pyplot as plt
+import json
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog, DatasetCatalog
+from detectron2.structures import BoxMode
+
 if __name__ == "__main__":
     from detectron2.evaluation import COCOEvaluator, inference_on_dataset
     from detectron2.data import build_detection_test_loader
